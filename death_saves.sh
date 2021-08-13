fe1120d0394be502434585d55a69070813ed0e20
Dominic Amann <dominic.amann@gmail.com>
2021-08-13 10:08:43 -0400

    initial commit

---
+++ death_saves.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+success=0
+failure=0
+successes=0
+failures=0
+
+for i in {0..10000..1}{
+do
+  while true
+  do
+    roll=$(( (${RANDOM} * 20 / 32768) + 1 ))
+    echo "Roll = $roll"
+    if  [ $roll -eq 1 ]
+    then
+      failure=$(( $failure + 2 ))
+    elif [ $roll -lt 10 ]
+    then
+     failure=$(( $failure + 1 )) 
+    elif [ $roll -eq 20 ] 
+    then
+      success=3
+    else
+      success=$(( $success + 1 ))
+    fi
+    if [ $failure -ge 3 ]
+    then
+      echo "You died"
+      failure=0
+      failures=$(( $failures + 1 ))
+      break
+    elif [ $success -ge 3 ]
+    then
+      echo "You recovered"
+      success=0
+      successes=$(( $successes + 1 ))
+      break
+    fi
+  done
+done
+
+echo "Total successes: $successes"
+echo "      failures:  $failures"   
